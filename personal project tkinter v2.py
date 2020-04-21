9c616680a2beb98eba3cdc87118d972cf81db118
Edward Lowe <edward@edwardtlowe.com>
2020-04-21 18:06:19 +0800

    scrolling listbox working

--- personal project tkinter v2.py
+++ personal project tkinter v2.py
@@ -50,24 +50,36 @@ def chooseAlgorithm(problemType,features,targets):
 
 class page(Frame):
 
-	def __init__(self, master,title,font=('Helvetica',30)):
-		super(page ,self).__init__()
+	def __init__(self, master,title,font=('Helvetica',60)):
+		super(page,self).__init__(master)
 
 		self.titleLabel = Label(self, text=title, font=font, relief=GROOVE)
-		self.titleLabel.grid(column=0, row=0, padx=20, pady=30, ipadx=40, ipady=30)
+		self.titleLabel.pack(pady=50,padx=30,ipadx=30,ipady=30)
 
 		self.contentFrame = Frame(self)
-		self.contentFrame.grid(column=0, row=1, padx=10, pady=10, ipadx=30, ipady=30)
+		self.contentFrame.pack()
 
-class scrollingListbox(Listbox):
+class scrollingListbox(Frame):
 
-	def __init__(self,master,selectmode=EXTENDED):
+	def __init__(self,master,height=13,width=40):
+		super(scrollingListbox,self).__init__(master)
 
-		self.scrollbar = Scrollbar(self,command=self.set)
-		self.config(yscrollcommand=self.scrollbar.yview)
+		self.listbox = Listbox(self,selectmode=EXTENDED,height=height,width=width)
+		self.listbox.grid(column=0,row=0)
 
+		self.scrollbar = Scrollbar(self)
+		self.scrollbar.grid(column=1,row=0,sticky=NS)
+
+		self.listbox.config(yscrollcommand=self.scrollbar.set)
+		self.scrollbar.config(command=self.listbox.yview)
+
+	def insert(self,index,item):
+		self.listbox.insert(index,item)
 
 window = tk.Tk()
+window.title('Machine Learning Creator')
+window.state('zoomed')
+window.config(bg='green')
 
 problemType = StringVar(value="Text (Classification) -- Default")
 continueVar = BooleanVar()
@@ -75,15 +87,15 @@ trainingDataDF = pd.DataFrame()
 
 # homepage
 HomePage = page(window,'Machine Learning Creator')
-HomePage.grid()
-startButton = Button(HomePage.contentFrame,text='Start',command=lambda:continueVar.set(True))
-startButton.grid()
+HomePage.pack(fill=BOTH,expand=1)
+startButton = Button(HomePage.contentFrame,text='Start',command=lambda:continueVar.set(True),font=('helvetica',30))
+startButton.grid(padx=20,pady=20)
 startButton.wait_variable(continueVar)
-HomePage.grid_forget()
+HomePage.pack_forget()
 
 # problem type selection
 ProblemSelect = page(window,'Problem Type')
-ProblemSelect.grid()
+ProblemSelect.pack(fill=BOTH,expand=1)
 explanationBox = Label(ProblemSelect.contentFrame)
 
 problemTypeChoices = {'Images (Classification)': 'Predict a label from an image',
@@ -103,49 +115,13 @@ explanationBox.grid(column=1, row=3, sticky=E)
 nxtBtn = Button(ProblemSelect.contentFrame, text="next",command=lambda:continueVar.set(True))
 nxtBtn.grid(column=0, columnspan=2, padx=10, ipadx=30, ipady=5)
 
-ProblemSelect.grid_forget()
+ProblemSelect.pack_forget()
 
 # select which columns to use
 DataCollecting = page(window,'Select Training Data')
-DataCollecting.grid()
+DataCollecting.pack(fill=BOTH,expand=1)
 
-# listbox with all the column names
-columnListbox = Listbox(DataCollecting.contentFrame,selectmode=EXTENDED)
-columnListbox.grid(column=0,row=0,rowspan=9)
-
-columnScrollbar = Scrollbar(columnListbox)
-columnScrollbar.grid()
-
-columnListbox.config(yscrollcommand=columnScrollbar.set)
-columnScrollbar.config(command=columnListbox.yview)
-
-
-featureAddButton = Button(DataCollecting.contentFrame,text='Add >>>')
-featureAddButton.grid(column=1,row=1)
-
-featureRemoveButton = Button(DataCollecting.contentFrame,text='<<< Remove')
-featureRemoveButton.grid(column=1,row=2)
-
-featureListbox = Listbox(DataCollecting.contentFrame,selectmode=EXTENDED)
-featureListbox.grid(column=2,row=0,rowspan=4,padx=10,pady=10)
-
-featureScrollbar = Scrollbar(featureListbox)
-featureScrollbar.grid()
-
-featureListbox.config(yscrollcommand=featureScrollbar.set)
-featureScrollbar.config(command=featureListbox.yview)
-
-
-targetAddButton = Button(DataCollecting.contentFrame,text='Add >>>')
-targetAddButton.grid(column=1,row=5)
-
-targetRemoveButton = Button(DataCollecting.contentFrame,text='<<< Remove')
-targetRemoveButton.grid(column=1,row=6)
-
-targetListbox = Listbox(DataCollecting.contentFrame,selectmode=EXTENDED)
-targetListbox.grid(column=2,row=4,rowspan=4,padx=10,pady=10)
-
-
+# load data
 filename = askopenfilename(initialdir='/', title='Choose Training Data', filetypes=[
 							('CSV', '*.csv'), ('Excel spreadsheets', '*.xls *.xlsx *.xlsm *.xlsb')])
 if path.splitext(filename)[1].lower() == '.csv':
@@ -155,12 +131,32 @@ else:
 
 if 'Classification' in problemType.get():
 	le = LabelEncoder()
-	le.fit(np.array(list(set(trainingDataDF.to_numpy().flatten().tolist()))).reshape(-1,1))
+	le.fit(np.array(list(set(trainingDataDF.to_numpy().flatten().tolist()))).reshape(-1,1).ravel())
 
-map(lambda columnName: columnListbox.insert(END,columnName),trainingDataDF.columns)
+# listbox with all the column names
+columnListbox = scrollingListbox(DataCollecting.contentFrame,25)
+columnListbox.grid(column=0,row=0,rowspan=9,padx=10,pady=10,sticky=NS)
+for columName in trainingDataDF.columns:
+	columnListbox.insert(END,columName)
 
-DataCollecting.grid_forget()
+
+featureAddButton = Button(DataCollecting.contentFrame,text='Add >>>')
+featureAddButton.grid(column=1,row=1)
+
+featureRemoveButton = Button(DataCollecting.contentFrame,text='<<< Remove')
+featureRemoveButton.grid(column=1,row=2)
+
+featureListbox = scrollingListbox(DataCollecting.contentFrame)
+featureListbox.grid(column=2,row=0,rowspan=4,padx=10,pady=10)
 
 
-window.title('Machine Learning Creator')
+targetAddButton = Button(DataCollecting.contentFrame,text='Add >>>')
+targetAddButton.grid(column=1,row=5)
+
+targetRemoveButton = Button(DataCollecting.contentFrame,text='<<< Remove')
+targetRemoveButton.grid(column=1,row=6)
+
+targetListbox = scrollingListbox(DataCollecting.contentFrame)
+targetListbox.grid(column=2,row=4,rowspan=4,padx=10,pady=10)
+
 window.mainloop()
